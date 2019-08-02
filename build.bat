092ff5599aa4459933aa45fd325c7b9ec366b9d6
Brad Robinson <contact@toptensoftware.com>
2019-08-02 15:46:50 +1000

    Build/versioning

---
+++ build.bat
@@ -0,0 +1,2 @@
+@echo off
+node build.js %*
---
+++ build.js
@@ -0,0 +1,37 @@
+var bt = require('./BuildTools/buildTools.js')
+
+// Used for generation version.* files
+bt.options.companyName = "Topten Software";
+
+// Load version info
+bt.version();
+
+if (bt.options.official)
+{
+    // Check everything committed
+    bt.git_check();
+
+    // Clock version
+    bt.clock_version();
+
+    // Force clean
+    bt.options.clean = true;
+    bt.clean("./Build");
+
+    // Run Tests
+    bt.dntest("Release", "Topten.RichText.Test");
+}
+
+// Build
+bt.dnbuild("Release", "Topten.RichText");
+
+if (bt.options.official)
+{
+    // Tag and commit
+    bt.git_tag();
+
+    // Push nuget package
+    bt.nupush(`./build/Release/Topten.RichText/*.${bt.options.version.build}.nupkg`, "http://nuget.toptensoftware.com/v3/index.json");
+}
+
+
