f23ac654ad29c91fc23ae4dcf1165fcbf751c84c
Brad Robinson <contact@toptensoftware.com>
2020-05-23 22:33:46 +1000

    Added StyleManager for easier creation and editing of styles

---
+++ Topten.RichTextKit/StyleManager.cs
@@ -0,0 +1,250 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Topten.RichTextKit
+{
+    /// <summary>
+    /// Helper class for managing RichTextKit styles.
+    /// </summary>
+    /// <remarks>
+    /// The StyleManager can be used to simplify the creation of styles by
+    /// maintaining a current style that incremental changes can be made to.
+    ///
+    /// eg: turn bold on, underline off, change font family etc...
+    /// 
+    /// The StyleManager also implements an internal stack to simplify applying
+    /// a particular style and then popping back to the previous style.
+    /// </remarks>
+    public class StyleManager
+    {
+        /// <summary>
+        /// The default style manager instance
+        /// </summary>
+        public static StyleManager Default = new StyleManager();
+
+        /// <summary>
+        /// Constructs a new StyleManager
+        /// </summary>
+        public StyleManager()
+        {
+            CurrentStyle = new Style();
+            _defaultStyle = _currentStyle;
+        }
+
+        /// <summary>
+        /// The current style
+        /// </summary>
+        public IStyle CurrentStyle
+        {
+            get => _currentStyle;
+            set => _currentStyle = FromStyle(value);
+        }
+
+        /// <summary>
+        /// The default style to be be used when Reset is called
+        /// </summary>
+        public IStyle DefaultStyle
+        {
+            get => _defaultStyle;
+            set => _defaultStyle = FromStyle(value);
+        }
+
+        /// <summary>
+        /// Get a style that matches all the style attributes of the supplied style
+        /// </summary>
+        /// <remarks>
+        /// This method creates a style owned by this style manager with all the same 
+        /// attributes as the passed style.
+        /// </remarks>
+        /// <param name="value">The style to copy</param>
+        /// <returns></returns>
+        public IStyle FromStyle(IStyle value)
+        {
+            return Update(value.FontFamily, value.FontSize, value.FontWeight, value.FontItalic,
+                            value.Underline, value.StrikeThrough, value.LineHeight, value.TextColor,
+                            value.FontVariant, value.TextDirection);
+        }
+
+        /// <summary>
+        /// Resets the current style to the default style and resets the internal
+        /// Push/Pop style stack to empty.
+        /// </summary>
+        public void Reset()
+        {
+            _currentStyle = _defaultStyle;
+            _userStack.Clear();
+        }
+
+        /// <summary>
+        /// Saves the current state on an internal stack 
+        /// </summary>
+        public void Push()
+        {
+            _userStack.Push(_currentStyle);
+        }
+
+        /// <summary>
+        /// Restores the current state on an internal stack 
+        /// </summary>
+        public void Pop()
+        {
+            _currentStyle = _userStack.Pop();
+        }
+
+        /// <summary>
+        /// Changes the font family and returns an updated IStyle
+        /// </summary>
+        /// <param name="fontFamily">The new font family</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle FontFamily(string fontFamily) => Update(fontFamily: fontFamily);
+
+        /// <summary>
+        /// Changes the font size and returns an updated IStyle
+        /// </summary>
+        /// <param name="fontSize">The new font size</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle FontSize(float fontSize) => Update(fontSize: fontSize);
+
+        /// <summary>
+        /// Changes the font weight and returns an updated IStyle
+        /// </summary>
+        /// <param name="fontWeight">The new font weight</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle FontWeight(int fontWeight) => Update(fontWeight: fontWeight);
+
+
+        /// <summary>
+        /// Changes the font weight and returns an update IStyle (short cut to FontWeight)
+        /// </summary>
+        /// <param name="bold">The new font weight</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle Bold(bool bold) => Update(fontWeight: bold ? 700 : 400);
+
+        /// <summary>
+        /// Changes the font italic setting and returns an updated IStyle
+        /// </summary>
+        /// <param name="fontItalic">The new font italic setting</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle FontItalic(bool fontItalic) => Update(fontItalic: fontItalic);
+
+        /// <summary>
+        /// Changes the underline style and returns an updated IStyle
+        /// </summary>
+        /// <param name="underline">The new underline style</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle Underline(UnderlineStyle underline) => Update(underline: underline);
+
+        /// <summary>
+        /// Changes the strikethrough style and returns an updated IStyle
+        /// </summary>
+        /// <param name="strikeThrough">The new strikethrough style</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle StrikeThrough(StrikeThroughStyle strikeThrough) => Update(strikeThrough: strikeThrough);
+
+        /// <summary>
+        /// Changes the line height and returns an updated IStyle
+        /// </summary>
+        /// <param name="lineHeight">The new line height</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle LineHeight(float lineHeight) => Update(lineHeight: lineHeight);
+
+        /// <summary>
+        /// Changes the text color and returns an updated IStyle
+        /// </summary>
+        /// <param name="textColor">The new text color</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle TextColor(SKColor textColor) => Update(textColor: textColor);
+
+        /// <summary>
+        /// Changes the font variant and returns an updated IStyle
+        /// </summary>
+        /// <param name="fontVariant">The new font variant</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle FontVariant(FontVariant fontVariant) => Update(fontVariant: fontVariant);
+
+        /// <summary>
+        /// Changes the text direction and returns an updated IStyle
+        /// </summary>
+        /// <param name="textDirection">The new text direction</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle TextDirection(TextDirection textDirection) => Update(textDirection: textDirection);
+
+
+        /// <summary>
+        /// Update the current style by applying one or more changes to the current
+        /// style.
+        /// </summary>
+        /// <param name="fontFamily">The new font family</param>
+        /// <param name="fontSize">The new font size</param>
+        /// <param name="fontWeight">The new font weight</param>
+        /// <param name="fontItalic">The new font italic</param>
+        /// <param name="underline">The new underline style</param>
+        /// <param name="strikeThrough">The new strike-through style</param>
+        /// <param name="lineHeight">The new line height</param>
+        /// <param name="textColor">The new text color</param>
+        /// <param name="fontVariant">The new font variant</param>
+        /// <param name="textDirection">The new text direction</param>
+        /// <returns>An IStyle for the new style</returns>
+        public IStyle Update(
+               string fontFamily = null,
+               float? fontSize = null,
+               int? fontWeight = null,
+               bool? fontItalic = null,
+               UnderlineStyle? underline = null,
+               StrikeThroughStyle? strikeThrough = null,
+               float? lineHeight = null,
+               SKColor? textColor = null,
+               FontVariant? fontVariant = null,
+               TextDirection? textDirection = null
+            )
+        {
+            // Resolve new style against current style
+            var rFontFamily = fontFamily ?? _currentStyle.FontFamily;
+            var rFontSize = fontSize ?? _currentStyle.FontSize;
+            var rFontWeight = fontWeight ?? _currentStyle.FontWeight;
+            var rFontItalic = fontItalic ?? _currentStyle.FontItalic;
+            var rUnderline = underline ?? _currentStyle.Underline;
+            var rStrikeThrough = strikeThrough ?? _currentStyle.StrikeThrough;
+            var rLineHeight = lineHeight ?? _currentStyle.LineHeight;
+            var rTextColor = textColor ?? _currentStyle.TextColor;
+            var rFontVariant = fontVariant ?? _currentStyle.FontVariant;
+            var rTextDirection = textDirection ?? _currentStyle.TextDirection;
+
+            // Format key
+            var key = $"{rFontFamily}.{rFontSize}.{rFontWeight}.{rFontItalic}.{rUnderline}.{rStrikeThrough}.{rLineHeight}.{rTextColor}.{rFontVariant}.{rTextDirection}";
+
+            // Look up...
+            if (!_styleMap.TryGetValue(key, out var style))
+            {
+                // Create a new style
+                style = new Style()
+                {
+                    FontFamily = rFontFamily,
+                    FontSize = rFontSize,
+                    FontWeight = rFontWeight,
+                    FontItalic = rFontItalic,
+                    Underline = rUnderline,
+                    StrikeThrough = rStrikeThrough,
+                    LineHeight = rLineHeight,
+                    TextColor = rTextColor,
+                    FontVariant = rFontVariant,
+                    TextDirection = rTextDirection,
+                };
+
+                // Add to map
+                _styleMap.Add(key, style);
+            }
+
+            // Set the new current style and return it
+            return _currentStyle = style;
+        }
+
+        Dictionary<string, Style> _styleMap = new Dictionary<string, Style>();
+        Stack<IStyle> _userStack = new Stack<IStyle>();
+        IStyle _defaultStyle = new Style();
+        IStyle _currentStyle;
+
+    }
+}
