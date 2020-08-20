5b1e38ae10747caf391c62375a1bec5c08c79d69
Brad Robinson <contact@toptensoftware.com>
2020-08-20 11:10:58 +1000

    Cleaning up line break trie generator

--- Topten.RichTextKit.Test/LineBreakTest.cs
+++ Topten.RichTextKit.Test/LineBreakTest.cs
@@ -82,6 +82,8 @@ namespace Topten.RichTextKit.Test
             var location = System.IO.Path.GetDirectoryName(typeof(LineBreakTests).Assembly.Location);
             var lines = System.IO.File.ReadAllLines(System.IO.Path.Combine(location, "LineBreakTest.txt"));
 
+            int failCount = 0;
+
             // Process each line
             int lineNumber = 0;
             foreach (var line in lines)
@@ -143,9 +145,29 @@ namespace Topten.RichTextKit.Test
                     foundBreaks.Add(b.PositionWrap);
                 }
 
+                if (!AreEqual(breakPoints, foundBreaks))
+                {
+                    failCount++;
+                    Console.WriteLine($"Failed: {lineNumber - 1}");
+                }
+
                 // Check the same
-                Assert.Equal(breakPoints, foundBreaks);
+                //Assert.Equal(breakPoints, foundBreaks);
             }
+
+            Assert.Equal(0, failCount);
+        }
+
+        bool AreEqual(List<int> a, List<int> b)
+        {
+            if (a.Count != b.Count)
+                return false;
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] != b[i])
+                    return false;
+            }
+            return true;
         }
 
         static bool IsHexDigit(char ch)
@@ -156,16 +178,8 @@ namespace Topten.RichTextKit.Test
         // these tests are weird, possibly incorrect or just tailored differently. we skip them.
         static HashSet<int> _skipLines = new HashSet<int>()
         {
-             812,  814,  848,  850,  864,  866,  900,  902,  956,  958, 1068, 1070, 1072, 1074, 1224, 1226,
-            1228, 1230, 1760, 1762, 2932, 2934, 4100, 4101, 4102, 4103, 4340, 4342, 4496, 4498, 4568, 4570,
-            4704, 4706, 4707, 4708, 4710, 4711, 4712, 4714, 4715, 4716, 4718, 4719, 4722, 4723, 4726, 4727,
-            4730, 4731, 4734, 4735, 4736, 4738, 4739, 4742, 4743, 4746, 4747, 4748, 4750, 4751, 4752, 4754,
-            4755, 4756, 4758, 4759, 4760, 4762, 4763, 4764, 4766, 4767, 4768, 4770, 4771, 4772, 4774, 4775,
-            4778, 4779, 4780, 4782, 4783, 4784, 4786, 4787, 4788, 4790, 4791, 4794, 4795, 4798, 4799, 4800,
-            4802, 4803, 4804, 4806, 4807, 4808, 4810, 4811, 4812, 4814, 4815, 4816, 4818, 4819, 4820, 4822,
-            4823, 4826, 4827, 4830, 4831, 4834, 4835, 4838, 4839, 4840, 4842, 4843, 4844, 4846, 4847, 4848,
-            4850, 4851, 4852, 4854, 4855, 4856, 4858, 4859, 4960, 4962, 5036, 5038, 6126, 6135, 6140, 6225,
-            6226, 6227, 6228, 6229, 6230, 6232, 6233, 6234, 6235, 6236, 6332,
+            1140, 1142, 1144, 1146, 1308, 1310, 1312, 1314, 2980, 2982, 4496, 4498, 4664, 4666, 5164, 5166,
+            7136, 7145, 7150, 7235, 7236, 7237, 7238, 7239, 7240, 7242, 7243, 7244, 7245, 7246
         };
-    }
+        }
 }
