e00a89e43392180a2eb22bc8848fb4904eb5f5dd
Brad Robinson <contact@toptensoftware.com>
2021-08-04 12:34:03 +1000

    b0.4.143

--- version.cs
+++ version.cs
@@ -3,13 +3,13 @@
 using System;
 using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2019-2021 Topten Software. All Rights Reserved")]
-[assembly: AssemblyVersion("0.4.142")]
-[assembly: AssemblyFileVersion("0.4.142")]
+[assembly: AssemblyVersion("0.4.143")]
+[assembly: AssemblyFileVersion("0.4.143")]
 [assembly: AssemblyCompany("Topten Software")]
 [assembly: AssemblyProduct("Topten.RichTextKit")]
 
 static class BuildInfo
 {
-	public static DateTime Date = new DateTime(2021, 8, 4, 1, 41, 32, DateTimeKind.Utc);
+	public static DateTime Date = new DateTime(2021, 8, 4, 2, 33, 14, DateTimeKind.Utc);
 }
 	
