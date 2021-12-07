7589173b06195c470df02b30eb3adbf8be117d2b
Brad Robinson <contact@toptensoftware.com>
2021-12-07 12:22:52 +1100

    Fixed build script error

--- build.js
+++ build.js
@@ -32,7 +32,7 @@ if (bt.options.official)
     bt.git_tag();
 
     // Push nuget package
-    var pushTo = bt.options.version.suffix ? "Topten GitHub" : "nuget.org";
+    var pushTo = bt.options.version.suffix ? "\"Topten GitHub\"" : "\"nuget.org\"";
     bt.run(`dotnet nuget push`,
            `./Build/Release/*.${bt.options.version.build}${bt.options.version.suffix}.nupkg`,
            `--source ${pushTo}`);
