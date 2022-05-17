b19c45ccd16e173336f16ab9cf19d2920138fd09
Brad Robinson <contact@toptensoftware.com>
2022-05-17 10:17:14 +1000

    Push to nuget again

--- build.js
+++ build.js
@@ -34,11 +34,9 @@ if (bt.options.official)
     // Push nuget package
 //    var pushTo = bt.options.version.suffix ? "\"Topten GitHub\"" : "\"nuget.org\"";
     var pushTo = "\"nuget.org\"";
-    /*
     bt.run(`dotnet nuget push`,
            `.\\Build\\Release\\*.${bt.options.version.build}${bt.options.version.suffix}.nupkg`,
            `--source ${pushTo}`);
-           */
 }
 
 
